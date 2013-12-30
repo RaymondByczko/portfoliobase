198191c81c031337131ee3edeb06dbbd42cf1517
Raymond Byczko <raymondbyczko@att.net>
2013-12-30 14:50:05 -0500

    self-RByczko; 2013-12-30 Dec 30; Initial work on BBC Healthcheck practice layout.

---
+++ public_html/portfolio/app/bbchealthcheck/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel=stylesheet type="text/css" href="css/styles.css"></link>
+</head>
+<body>
+<div id="body_id">
+<div id="orange_id">
+<span>ora</span>
+</div>
+<div id="bbcpd_help_id">
+<div id="bbcpod_and_downloads_id">
+<a href="http://www.bbc.co.uk/podcasts">PODCASTS & DOWNLOADS</a>
+</div>
+<div id="bbc_blank_id">
+</div>
+<div id="bbc_help_id">
+<span>Help</span>
+</div>
+</div>
+<div id="find_podcast_id">
+<div id="find_part1_id">Find a podcast by radio station</div>
+<div id="find_part2_id"><form>Find Podcast:<input value="Type it in" type="text" id="quickfind_id" name="pdname"></form></div>
+</div>
+
+
+<div id="house1">
+</div>
+<div id="fountain1">
+</div>
+<div id="rocket_border">
+<div id="rocket">
+</div>
+</div>
+<div id="movie_border">
+<div id="clip_border" class="clipedge">
+<layer id="layer1">
+<img id="motorguy"
+class="transp" src="images/transparent.png">
+</layer>
+<layer id="layer2">
+<div id="building">
+</div>
+</layer>
+</div>
+</div>
+</div>
+
+<script>
+function startanimation()
+{
+	document.getElementById("motorguy").style.animationPlayState = 'running';
+	document.getElementById("motorguy").style.webkitAnimationPlayState = 'running';
+}
+</script>
+</body>
+</html>
